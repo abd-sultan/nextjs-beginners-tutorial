7d4fe3a362677fb40e3d2395bc0221d8ae438116
hassim506 <hassimiou061@gmail.com>
2025-01-25 19:44:17 +0100

    Methode (GET)  de récupération des produits depuis l'API reqress en utilisant axios

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,25 +1,7 @@
-import {  NextResponse } from "next/server";
+import axios from "axios";
+import { NextResponse } from "next/server";
 
-export async function GET (){
-    return NextResponse.json({
-
-         product: [
-            {
-                id: 1,
-                name: "Produit 1",
-                price: 10,
-                description: "Description du produit 1"
-            },
-         ]
-        
-        
-        }); 
-    
-}
-export async function POST(request: Request){
-    const data = await request.json();
-    return NextResponse.json({
-        data
-    });
-    
+export async function GET(){
+    const response = await axios.get("https://reqres.in/api/products");
+    return NextResponse.json(response.data)
 }
