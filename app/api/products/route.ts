c7f3989b53496d4277adc05b6dec570feda20295
hassim506 <hassimiou061@gmail.com>
2025-01-25 21:55:51 +0100

    auto-commit

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { NextResponse } from "next/server";
 
+// Methode (GET)  de récupération des produits depuis l'API reqress en utilisant axios
 export async function GET(){
     const response = await axios.get("https://reqres.in/api/products");
     return NextResponse.json(response.data)
