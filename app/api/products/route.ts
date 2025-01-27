cb44f41772e67ac77b6bb397a1d2a7a3d742109c
hassim506 <hassimiou061@gmail.com>
2025-01-27 14:07:43 +0100

    Méthode d'ajout des produits dans l'api avec post

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,8 +1,23 @@
 import axios from "axios";
-import { NextResponse } from "next/server";
+import { NextApiRequest } from "next";
+import { NextRequest, NextResponse } from "next/server";
 
 // Methode (GET)  de récupération des produits depuis l'API reqress en utilisant axios
 export async function GET(){
     const response = await axios.get("https://reqres.in/api/products");
     return NextResponse.json(response.data)
+}
+
+//Methode (POST) d'ajout d'un nouveau produit à l'API reqress en utilisant axios
+export async function POST(req: NextRequest){
+    try{
+        const body = await req.json();
+    console.log("Requête réçu depuis L\'API", body);
+    const response = await axios.post("https://reqres.in/api/products", body);
+    console.log("Reponse de L\'API", response.data);
+    return NextResponse.json(response.data);
+    } catch(error){
+        console.error("Erreur lors de l'ajout du produit :", error);
+        return NextResponse.json({error: "Erreur lors de l'ajout du produit", details: error.message}, {status: 500});
+    }
 }
